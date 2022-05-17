4aebcf03da901887c2092a9fc8cf876f167ea0c8
ArthursLesinsh <lesinsh@gmail.com>
2022-05-17 20:11:54 +0300

    implemented comment deletion functionality

---
+++ public/api.php
@@ -0,0 +1,47 @@
+<?php
+
+include __DIR__ . '/../private/bootstrap.php';
+
+use Storage\DB;
+
+header('Content-Type: application/json');
+$output = ['status' => false];
+if (isset($_GET['name']) && is_string($_GET['name'])) {
+    if ($_GET['name'] === 'add-comment') {
+        if (
+            isset($_POST['author']) && is_string($_POST['author']) &&
+            isset($_POST['message']) && is_string($_POST['message'])
+        ) {
+            $comment_manager = new DB('comments');
+            $output = [
+                'status' => true,
+                'author' => $_POST['author'],
+                'message' => $_POST['message'],
+                'id' => $comment_manager->addEntry([
+                    'author' => $_POST['author'],
+                    'message' => $_POST['message']
+                ])
+            ];
+        }
+    }
+    elseif ($_GET['name'] === 'get-comments') {
+        $comment_manager = new DB('comments');
+        $output = [
+            'status' => true,
+            'comments' => $comment_manager->getAll()
+        ];
+    }
+    elseif ($_GET['name'] === 'delete-comment') {
+        if (isset($_POST['id']) && is_string($_POST['id'])) {
+            $comment_manager = new DB('comments');
+            $id = (int) $_POST['id'];
+
+            $output = [
+                'status' => $comment_manager->deleteEntry($id),
+                'id' => $id,
+            ];
+        }
+    }
+}
+
+echo json_encode($output, JSON_PRETTY_PRINT);
--- public/script.js
+++ public/script.js
@@ -2,19 +2,34 @@ const form = document.getElementById('comments_form');
 const comment_block = document.querySelector('.comments');
 const comment_template = comment_block.querySelector('.template');
 
+xhttp.get('api.php?name=get-comments', function (response) {
+    for (let comment of response.comments) {
+        addComment(comment.id, comment.author, comment.message);
+    }
+});
+
 form.onsubmit = function (event) {
     event.preventDefault();
-    const data = new FormData(this);
-    addComment(data.get('author'), data.get('message'));
-
-    this.reset();
+    xhttp.postForm(this, function (response) {
+        addComment(response.id, response.author, response.message);
+    });
 };
 
-function addComment(author, message) {
+function addComment(id, author, message) {
     const new_comment = comment_template.cloneNode(true);
     new_comment.classList.remove('template');
     new_comment.querySelector('.message').textContent = message;
     new_comment.querySelector('.author').textContent = author;
+    new_comment.dataset.id = id;
+
+    new_comment.querySelector('.delete').onclick = function (event) {
+        const data = new FormData();
+        data.set('id', id);
+
+        xhttp.post('api.php?name=delete-comment', data, function (response) {
+            new_comment.remove();
+        });
+    };
 
     comment_block.append(new_comment)
 }
