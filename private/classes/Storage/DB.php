21c55c4b9bee49117ef4d55ad522b0af61e2072b
ArthursLesinsh <lesinsh@gmail.com>
2022-05-19 21:40:56 +0300

    DB.api update

--- private/classes/Storage/DB.php
+++ private/classes/Storage/DB.php
@@ -5,7 +5,7 @@ class DB
 {
     private $table_name = null;
     private $connection = null;
-    public function __construct($table_name) {
+    public function __construct(string $table_name) {
         $this->table_name = $table_name;
         $this->conn = new \mysqli(DB_SERVER_NAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
         if ($this->conn->connect_error) {
@@ -17,7 +17,7 @@ class DB
         $this->conn->close();
     }
     
-    public function addEntry($entry) {
+    public function addEntry(array $entry) {
         $column_str = '';
         $value_str = '';
         foreach ($entry as $key => $value) {
@@ -36,6 +36,22 @@ class DB
         return false;
     }
 
+    public function updateEntry(int $id, array $entry) {
+        $column_value_str = '';
+        foreach ($entry as $key => $value) {
+            $column_value_str .= $key . "=" . "' " . $this->conn->real_escape_string($value) . " ',";
+        }
+        $column_value_str = rtrim($column_value_str, ',');
+
+        $sql = "UPDATE " . $this->table_name . " SET $column_value_str WHERE id=$id";
+
+        $result = $this->conn->query($sql);
+        if ($result === true) {
+            return $entry;
+        }
+        return false;
+    }
+
     public function getAll() {
         $sql = "SELECT * FROM " . $this->table_name;
         $result = $this->conn->query($sql);
@@ -43,8 +59,15 @@ class DB
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    public function getEntry(int $id) {
+        $sql = "SELECT * FROM " . $this->table_name . " WHERE id=$id";
+        $result = $this->conn->query($sql);
+
+        return $result->fetch_assoc();
+    }
+
     public function deleteEntry(int $id) {
-        $sql = "DELETE FROM " . $this->table_name . " WHERE id=" . $id;
+        $sql = "DELETE FROM " . $this->table_name . " WHERE id=$id";
 
         return ($this->conn->query($sql) === true);
     }
