8f916b74dde39ac41a986cef07d99334c6b213a8
ArthursLesinsh <lesinsh@gmail.com>
2022-05-20 21:12:43 +0300

    upload image done function

--- private/bootstrap.php
+++ private/bootstrap.php
@@ -1,18 +1,19 @@
 <?php
 
 define('DEBUG_MODE', true);
-
-if (DEBUG_MODE) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-}
+define('PRIVATE_DIR', __DIR__ . '/');
+define('UPLOAD_DIR', PRIVATE_DIR . 'uploads/');
 
 define('DB_SERVER_NAME', 'localhost');
 define('DB_NAME', 'bootcamp');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 
+if (DEBUG_MODE) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
 spl_autoload_register(function ($class) {
     $file = __DIR__ . '/classes/' . str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
